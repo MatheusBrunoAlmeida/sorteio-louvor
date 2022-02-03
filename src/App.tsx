1a387afd98698b3899ace9fa8134a2b3152406e0
matheusalmeidarock <matheus.almeida@rockcontent.com>
2022-02-03 18:02:58 -0300

    hello wolrd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, CardTomTitle, CardTom, Title, ButtonSortearTom } from './style';
 
 function App() {
+  const [tom, setTom] = useState('Tom')
+  const [tomSorteio, setTomSorteio] = useState({})
+
+  const tons = [
+    {
+      id: 1,
+      tom: 'C'
+    },
+    {
+      id: 2,
+      tom: 'C#'
+    },
+    {
+      id: 3,
+      tom: 'D'
+    },
+    {
+      id: 4,
+      tom: 'Eb'
+    },
+    {
+      id: 5,
+      tom: 'E'
+    },
+    {
+      id: 6,
+      tom: 'F'
+    },
+    {
+      id: 7,
+      tom: 'F#'
+    },
+    {
+      id: 8,
+      tom: 'G'
+    },
+    {
+      id: 9,
+      tom: 'G#'
+    },
+    {
+      id: 10,
+      tom: 'A'
+    },
+    {
+      id: 11,
+      tom: 'Bb'
+    },
+    {
+      id: 12,
+      tom: 'B'
+    },
+  ]
+
+  function sortearTom(){
+    const tomSortear = Math.floor(Math.random() * 12);
+    console.log(tomSortear)
+
+    let resultado = tons.find(tom => tom.id == tomSortear)
+
+    defineTom(resultado)
+  }
+
+  function defineTom(resultado:any){
+    setTom(resultado.tom)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box>
+        <Title>Sorteio Louvor</Title>
+
+        <CardTom>
+          <CardTomTitle> {tom} </CardTomTitle>
+        </CardTom>
+
+        <ButtonSortearTom onClick={sortearTom}>Sortear Tom</ButtonSortearTom>
+      </Box>
   );
 }
 
---
+++ src/style.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const Box = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background-color: #0e1117;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const Title = styled.h1`
+    color: #ffff;
+    margin-top: 2rem;
+    font-size: 18px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`
+export const CardTom = styled.div`
+    background-color: #161b22;
+    width: 300px;
+    margin-top: 3rem;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 2rem;
+    border-radius: 8px;
+`
+
+export const CardTomTitle = styled.h1`
+    color: #dbdbdb;
+    font-size: 18px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`
+
+export const ButtonSortearTom = styled.button`
+    background-color: #8257e5;
+    border: none;
+    margin-top: 1rem;
+    font-size: 14px;
+    width: 150px;
+    padding: 0.8rem 0.5rem 0.8rem 0.5rem;
+    color: #d9d6d6;
+    border-radius: 5px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 600;
+`
